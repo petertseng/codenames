2e5d8aae6eb80fda41f0e9d05a4736c84f8244a2
Peter Tseng <pht24@cornell.edu>
2015-10-25 19:50:12 -0700

    rspec: Disable monkey patching, enable warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ SimpleCov.start { add_filter '/spec/' }
 
 require 'codenames/game'
 
+RSpec.configure { |c|
+  c.warnings = true
+  c.disable_monkey_patching!
+}
+
 def example_game(num_players)
   g = Codenames::Game.new('test')
   num_players.times { |i| g.add_player("p#{i + 1}") }
